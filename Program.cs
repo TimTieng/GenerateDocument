27d2bb3b2d7809a52139660851098098181d595b
TimTieng <timothy.tieng@gmail.com>
2021-04-26 08:57:26 -0400

    Created primary and helper function to count frequency

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace GenerateDocumentName
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            // Test 1 - Positive Output
+            string test1 = "Bste!hetsi ogEAxpelrt x ";
+            string document1 = "AlgoExpert is the Best!";
+            GenerateDocumentName(test1, document1);
+        }
+        // Helper Function to count the amount of times a character is present
+        public static int FrequencyCounter (char character, string input)
+        {
+            // Declare and Init. Counter variable
+            int frequency = 0;
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                char c = input[i];
+                if (c == character)
+                {
+                    frequency++;
+                }
+            }
+            return frequency;
+        }
+        // Primary Function that meets problems endstate
+        public void GenerateDocumentName(string characters, string documentName)
+        {
+            for (int i = 0; i < documentName.Length; i++)
+            {
+                char character =documentName[i];
+
+                // Determine how many times each character is present in both document and characters array
+                int documentCharFrequency = FrequencyCounter(character, documentName);
+                int characterFrequency = FrequencyCounter(character, characters);
+
+                // Evaluate
+                if (documentCharFrequency > characterFrequency)
+                {
+                    Console.WriteLine("The document name cannot be generated with the list of characters Provided");
+                }
+            }
+            Console.WriteLine("The Document Name can be generated with the list of characters provided");
+        }
+    }
+}
